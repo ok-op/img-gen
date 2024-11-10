ac255229ac79dff27a3d8da72109ccafda04f39b
jaan op <125698743+ok-op@users.noreply.github.com>
2024-11-10 20:38:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const fetch = require("node-fetch");
+import express from 'express';
+import fetch from 'node-fetch'; // Using import instead of require
 const app = express();
 const PORT = 3000;
 
