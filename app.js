1d293ce208f4447ad2308216aa6d1169ab9ebd2b
jaan op <125698743+ok-op@users.noreply.github.com>
2024-11-10 21:34:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.get('/download', async (req, res) => {
     }
 });
 
+// Serve index.html at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Start the Express server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
