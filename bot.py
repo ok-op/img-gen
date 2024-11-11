6c3034e8a21ad695794d92d0b8b6c092d5d3246e
jaan op <125698743+ok-op@users.noreply.github.com>
2024-11-11 06:42:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,4 +103,5 @@ def download_file(filename):
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    port = int(os.getenv("PORT", 3000))
+    app.run(debug=True, host="0.0.0.0", port=port)
